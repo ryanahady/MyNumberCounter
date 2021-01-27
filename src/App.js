ce7059651f9cd02ec485de25c44f5cb080b1503f
Ryan A <ryan.zad@gmail.com>
2021-01-27 13:36:23 +1100

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+
+
 function App() {
+  // var count = 0;
+  
+  const [count, setCount] = useState(0); //useState hook
+
+  // function multiplyTwo(){
+  //   setCount(count * 2);
+  // }
+
+  function increase(){
+    setCount(count + 1);
+  }
+
+  function decrease(){
+    setCount(count - 1);
+  }
+
+  function reset(){
+    setCount(0);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{count}</h1>
+      {/* <button onClick={multiplyTwo}>*2</button> */}
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+      <button onClick={reset}>C</button>
     </div>
   );
+
 }
 
 export default App;
